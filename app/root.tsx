15cdd7b79819c9dcfe1c9ad2f1eccb212fef80cf
scorder96 <scorder96@gmail.com>
2025-02-25 19:53:30 +0530

    style: font style to Outfit

--- app/root.tsx
+++ app/root.tsx
@@ -24,12 +24,16 @@ export const links: LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;600;700&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body>
         {children}
