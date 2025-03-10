fbe380fb93ddbc8a13f109b040d3d1340467fddd
scorder96 <scorder96@gmail.com>
2025-03-10 20:28:40 +0530

    feat: free plan error div

---
+++ app/routes/pricing.free.tsx
@@ -0,0 +1,7 @@
+export default function FreeDialog() {
+  return (
+    <div className="bg-red-100 mt-4 p-4">
+      The free plan is limited to <b>3 posts</b> and <b>1 profile</b>.
+    </div>
+  );
+}
--- app/routes/pricing.tsx
+++ app/routes/pricing.tsx
@@ -1,3 +1,4 @@
+import { Outlet } from "@remix-run/react";
 import Footer from "~/components/Footer";
 import Navbar from "~/components/Navbar";
 import Pricing from "~/components/Pricing";
@@ -7,11 +8,12 @@ export default function pricing() {
   return (
     <>
       <Navbar />
-      <div className="px-8 md:px-32 mt-32 flex flex-col items-center">
+      <div className="px-8 md:px-32 mt-16 flex flex-col items-center">
         <h1 className="text-4xl font-bold text-center">
           Truly mind-blowing 🤯
         </h1>
         <p className="mt-4 opacity-50">Pay once, use forever</p>
+        <Outlet />
         <Reveal>
           <Pricing />
         </Reveal>
